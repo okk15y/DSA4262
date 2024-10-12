74ff3ff1d55ac0ab6971337b3481eb626a5ed253
okk15y <e0725809@u.nus.edu>
2024-10-12 18:41:45 +0800

    updated prediction and prediction_model script to fit new model

--- prediction.py
+++ prediction.py
@@ -2,7 +2,10 @@ import gzip
 import json
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from tensorflow.keras.models import load_model
+
 
 def load_json_gz_to_dataframe(file_path, num_lines=0):
     '''
@@ -27,11 +30,74 @@ def load_json_gz_to_dataframe(file_path, num_lines=0):
                             })
     return pd.DataFrame(data)
 
+
 def data_processing(dataset):
     transcriptID_position = dataset[['transcript_id', 'position']]
+
+
+    # Get mean data
     dataset['data'] = dataset['data'].apply(lambda x: np.mean(x, axis=0))
+
+    
+    # Standardize scale
+
+    # Extract the lists from the 'data' column
+    data_lists = np.vstack(dataset['data'].values)
+
+    # Initialize the StandardScaler
+    scaler = StandardScaler()
+
+    # Fit and transform the data
+    data_scaled = scaler.fit_transform(data_lists)
+
+    # Replace the original 'data' column with the scaled data
+    dataset['data'] = list(data_scaled)
+
+    
+    # Extract the middle 5-mers sequence
+    dataset['sequence'] = dataset['sequence'].apply(lambda x: x[1:-1])
+
+    
+    # One-hot encoding
+    
+    # Define the possible nucleotides for each position
+    D = ['A', 'G', 'T']
+    R = ['A', 'G']
+    A = ['A']
+    C = ['C']
+    H = ['A', 'C', 'T']
+    
+    # Initialize an empty list to store the DRACH motifs
+    drach_motifs = []
+    
+    # Generate all combinations using nested loops
+    for d in D:
+        for r in R: 
+            for a in A:
+                for c in C:
+                    for h in H:
+                        motif = d + r + a + c + h
+                        drach_motifs.append(motif)
+
+    # Initialize the OneHotEncoder with DRACH motifs
+    encoder = OneHotEncoder(categories=[drach_motifs])
+
+    # Fit the encoder to 'sequence' and transform it into a one-hot encoded matrix
+    one_hot_matrix = encoder.fit_transform(dataset[['sequence']])
+
+    # Convert the one-hot encoded matrix into a column
+    one_hot_column = pd.Series([list(row) for row in one_hot_matrix.toarray()])
+
+    # Concatenate the one-hot encoded column to dataset
+    df_encoded = pd.concat([dataset, one_hot_column.rename('one_hot_encoded')], axis=1)
+
+    # Combine 'data' and 'one_hot_encoded' into a single list for each row
+    df_encoded['combined'] = df_encoded.apply(lambda x: x['data'].tolist() + x['one_hot_encoded'], axis=1)
+
     dataset = np.vstack(dataset["data"].values)
+
     return dataset, transcriptID_position
+
 
 def predict(dataset, transcriptID_position, model):
     predictions = model.predict(dataset)
@@ -40,14 +106,16 @@ def predict(dataset, transcriptID_position, model):
     predictions_df.rename(columns={'position': 'transcript_position'}, inplace=True)
     predictions_df.to_csv('predictions.csv', index=False)
 
+
 def main(file_path):
     # change this to name of model
-    model = load_model('final_model.keras')
+    model = load_model('best_model.keras')
     
     input_data = load_json_gz_to_dataframe(file_path)
     input_data, transcriptID_position = data_processing(input_data)
     
     predict(input_data, transcriptID_position, model)
+
 
 if __name__ == "__main__":
     input_file = input("File Path of json.gz dataset: ")
--- prediction_model.py
+++ prediction_model.py
@@ -3,9 +3,11 @@ import json
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 from tensorflow.keras.models import load_model
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.layers import Dense, Dropout, Input
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 
@@ -37,9 +39,70 @@ def load_labels(file_path):
     return pd.read_csv(file_path)
 
 
-def combine_data(dataset, labels):
+def data_processing(dataset):
+    # Get mean data
     dataset['data'] = dataset['data'].apply(lambda x: np.mean(x, axis=0))
+
     
+    # Standardize scale
+
+    # Extract the lists from the 'data' column
+    data_lists = np.vstack(dataset['data'].values)
+
+    # Initialize the StandardScaler
+    scaler = StandardScaler()
+
+    # Fit and transform the data
+    data_scaled = scaler.fit_transform(data_lists)
+
+    # Replace the original 'data' column with the scaled data
+    dataset['data'] = list(data_scaled)
+
+    
+    # Extract the middle 5-mers sequence
+    dataset['sequence'] = dataset['sequence'].apply(lambda x: x[1:-1])
+
+    
+    # One-hot encoding
+    
+    # Define the possible nucleotides for each position
+    D = ['A', 'G', 'T']
+    R = ['A', 'G']
+    A = ['A']
+    C = ['C']
+    H = ['A', 'C', 'T']
+    
+    # Initialize an empty list to store the DRACH motifs
+    drach_motifs = []
+    
+    # Generate all combinations using nested loops
+    for d in D:
+        for r in R: 
+            for a in A:
+                for c in C:
+                    for h in H:
+                        motif = d + r + a + c + h
+                        drach_motifs.append(motif)
+
+    # Initialize the OneHotEncoder with DRACH motifs
+    encoder = OneHotEncoder(categories=[drach_motifs])
+
+    # Fit the encoder to 'sequence' and transform it into a one-hot encoded matrix
+    one_hot_matrix = encoder.fit_transform(dataset[['sequence']])
+
+    # Convert the one-hot encoded matrix into a column
+    one_hot_column = pd.Series([list(row) for row in one_hot_matrix.toarray()])
+
+    # Concatenate the one-hot encoded column to dataset
+    df_encoded = pd.concat([dataset, one_hot_column.rename('one_hot_encoded')], axis=1)
+
+    # Combine 'data' and 'one_hot_encoded' into a single list for each row
+    df_encoded['combined'] = df_encoded.apply(lambda x: x['data'].tolist() + x['one_hot_encoded'], axis=1)
+
+    return dataset
+
+
+def combine_data(dataset, labels):
     merged_df = pd.merge(dataset, labels,
                      left_on  = ['transcript_id', 'position'],
                      right_on = ['transcript_id', 'transcript_position'],
@@ -66,8 +129,11 @@ def model_building(X_resampled, y_resampled):
     # Create a sequential model
     model = Sequential()
 
-    # Input layer (input_shape should match your feature count, which is 9)
-    model.add(Dense(150, activation='relu', input_shape=(9,)))  # First hidden layer
+    # Input layer
+    model.add(Input(shape=(X_resampled.shape[1],)))
+
+    # First hidden layer
+    model.add(Dense(150, activation='relu'))  # First hidden layer
     model.add(Dropout(0.2))  # Optional: dropout layer for regularization
 
     # Second hidden layer
@@ -81,16 +147,17 @@ def model_building(X_resampled, y_resampled):
     checkpoint = ModelCheckpoint('best_model.keras', monitor='val_accuracy', save_best_only=True, mode='max', verbose=1)
 
     # Fit the model to the training data
-    model.fit(X_resampled, y_resampled, epochs=10, batch_size=32, validation_split=0.2)  # Adjust epochs and batch_size as needed
+    model.fit(X_resampled, y_resampled, epochs=10, batch_size=32, validation_split=0.2, callbacks=[checkpoint])  # Adjust epochs and batch_size as needed
 
     # After fitting, you can save the model if required
-    model.save('final_model.keras')
+    model.save('best_model.keras')  
 
 
 def main(data_file, labels_file):
     dataset = load_json_gz_to_dataframe(data_file)
     labels = load_labels(labels_file)
 
+    dataset = data_processing(dataset)
     merged_df = combine_data(dataset, labels)
 
     X_test, y_test = X_y_train(merged_df)
